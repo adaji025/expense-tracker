ea11917a2148d843d64fc4497a3445cd68ec4666
Adaji Mukhtar <alfadaji@gmail.com>
2022-02-25 07:09:41 +0100

    project setup

---
+++ src/compnents/Balance/Balance.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Balance = () => {
+  return (
+    <div>
+        <h1>Balance</h1>
+        <p>₦80000</p>
+    </div>
+  )
+}
+
+export default Balance
---
+++ src/compnents/IncomeExpense/IncomeExpense.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const IncomeExpense = () => {
+  return (
+    <div className='expenseContainer'>
+        <div>
+        <h4>Income</h4>
+        <p>₦800</p>
+        </div>
+        <div>
+            <h4>Expense</h4>
+            <p>₦800</p>
+        </div>
+    </div>
+  )
+}
+
+export default IncomeExpense
